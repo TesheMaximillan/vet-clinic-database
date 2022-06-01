c611eff83900e0ed1a42d83fb1c972ba2f16ee20
Teshome Kurabachew <dear.teshe@gmail.com>
2022-06-01 22:22:33 +0000

    Rollback with savepoint

--- queries.sql
+++ queries.sql
@@ -26,3 +26,12 @@ TRUNCATE animals;
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+SELECT * FROM animals;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
